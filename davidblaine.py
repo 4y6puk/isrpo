308ef61cf8e95ed6f38bd541b59fa390f4b1d9d4
4y6puk <serbatyhvladimir608@gmail.com>
2024-12-19 12:51:25 +0300

    Add files via upload
    1 more task

---
+++ davidblaine.py
@@ -0,0 +1,7 @@
+import random
+def dblane_func(n):
+    return 1
+
+for i in range(4):
+    dblane_func(random.randint(1,10))  
+print(dblane_func(random.randint(1,10)))
---
+++ davidblaine_test.py
@@ -0,0 +1,7 @@
+from davidblaine import dblane_func
+
+def test_dblane_func():
+    assert dblane_func(2) == 1
+    assert dblane_func(10) == 1
+    assert dblane_func(5) == 1
+    assert dblane_func(7) == 1
